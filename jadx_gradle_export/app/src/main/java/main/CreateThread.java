9185e34d86122df99c50586970ff489b5cb860f1
huythanh0x <huythanh0x@gmail.com>
2026-01-01 22:20:59 +0700

    Fix Create Thread

--- jadx_gradle_export/app/src/main/java/main/CreateThread.java
+++ jadx_gradle_export/app/src/main/java/main/CreateThread.java
@@ -6,24 +6,36 @@ public class CreateThread extends Thread {
 
     public CreateThread(GameRun gr_, int type) {
         this.gr = gr_;
-        this.gr.threadType = (byte) type;
-        new Thread(this).start();
+        GameRun gr = this.gr;
+        byte threadType = (byte) type;
+        gr.threadType = threadType;
+        Thread thread = new Thread(this);
+        thread.start();
     }
 
     @Override // java.lang.Thread, java.lang.Runnable
     public void run() {
         try {
             this.gr.createOver = (byte) 0;
-            if (this.gr.threadType == 0) {
+            byte threadType = this.gr.threadType;
+            if (threadType == 0) {
                 this.gr.map.initMap();
                 this.gr.loadItem();
                 this.gr.loadMon(0, this.gr.monster_pro);
                 this.gr.loadMon(1, this.gr.monster_pro);
                 this.gr.loadInfoList();
-            } else if (this.gr.threadType == 1) {
-                this.gr.initBattle();
-            } else if (this.gr.threadType != 2 && this.gr.threadType != 3 && this.gr.threadType != 4) {
-                byte b = this.gr.threadType;
+            } else {
+                byte one = 1;
+                if (threadType == one) {
+                    this.gr.initBattle();
+                } else {
+                    byte two = 2;
+                    byte three = 3;
+                    byte four = 4;
+                    if (threadType != two && threadType != three && threadType != four) {
+                        byte b = threadType;
+                    }
+                }
             }
             this.gr.createOver = (byte) -1;
         } catch (Exception e) {
